88cf2f1f8639b559d2abc442495d518b5bc020b9
nickespro130 <nickespro130@outlook.es>
2024-09-12 13:14:34 +0000

    Implementation created

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Configura tu red Wi-Fi
+const char* ssid = "vodafone5990";
+const char* password = "KTXCJN4MDZXQ2Y";
+
+// Crea un servidor web en el puerto 80
+ESP8266WebServer server(37842);
+
+// Función para manejar la ruta raíz "/"
+void handleRoot() {
+  server.send(200, "text/plain", "pong");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200); // Inicia la comunicación serial
+  WiFi.begin(ssid, password); // Conéctate a la red Wi-Fi
+
+  Serial.print("Conectando a WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Conectado a la red Wi-Fi");
+  Serial.print("Dirección IP: ");
+  Serial.println(WiFi.localIP());
+
+  // Configura las rutas de la API
+  server.on("/", handleRoot);
+  server.begin(); // Inicia el servidor
+  Serial.println("Servidor web iniciado");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  server.handleClient(); // Maneja las solicitudes de los clientes
 }
